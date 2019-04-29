ab7eda8bbf49c38224bb3951d51c342d778fc364
Lan Nguyen <nguyenlan13@gmail.com>
2019-04-29 04:30:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,7 +39,7 @@ def sum_array (array)
 end
 
 def add_s (array)
-  array.each_with_index.collect{|element, index| if #{element}!= 'feet'}
+  array.each_with_index.collect{|element, index| if #{element}!=[1]}
    "#{element}s"
   end
   
